ad9ba4b1acbbe66da11bc86b46b6132fe6f2ff36
Wesley Lopes <wesley.lopes0902@gmail.com>
2023-03-14 00:43:57 -0300

    Teste 2

--- ImprimindoPositivosMedia/src/PositivosMedia.java
+++ ImprimindoPositivosMedia/src/PositivosMedia.java
@@ -1,36 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class PositivosMedia {
     public static void main(String[] args) throws Exception {
-        System.out.println("Teste!");
-    }
-
-    /*// Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma: 
-    // - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next";
-    // - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.  
-
-
-    import java.io.IOException;
-    import java.util.Scanner;
-
-    public class DIO {
-    public static void main(String[] args) throws IOException {
+        // System.out.println("Teste!");
         Scanner leitor = new Scanner(System.in);
+
         int cont = 0;
         double media = 0;
         double x = 0;
         for (int i = 1; i < 7; i++) {
-        System.out.println("Digite um valor: ");
-        double num = leitor.nextDouble();
+            System.out.println("Digite um valor: ");
+            double num = leitor.nextDouble();
 
-        if(num >= 0){
-            cont++;
-            x = x + num;
-        }
+            if (num >= 0) {
+                cont++;
+                x = x + num;
+            }
         }
 
         media = x / cont;
-        System.out.println(cont + " valores positivos");
-        System.out.println(String.format("%.1f", media));
+        System.out.println("valores positivos: " + cont);
+        System.out.println(String.format("Média: %.1f", media));
     }
-    }
-    */
 }
