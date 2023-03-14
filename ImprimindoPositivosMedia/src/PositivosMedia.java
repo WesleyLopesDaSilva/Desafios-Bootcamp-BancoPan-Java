524175d1ccf9df8fb4647c46dbebb91d29c52eef
Wesley Lopes <wesley.lopes0902@gmail.com>
2023-03-14 00:49:59 -0300

    Versão Final

--- ImprimindoPositivosMedia/src/PositivosMedia.java
+++ ImprimindoPositivosMedia/src/PositivosMedia.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class PositivosMedia {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         // System.out.println("Teste!");
         Scanner leitor = new Scanner(System.in);
 
@@ -24,3 +24,31 @@ public class PositivosMedia {
         System.out.println(String.format("Média: %.1f", media));
     }
 }
+
+/*
+    Versão correta para a plataforma DIO:(a única diferença é que não tem o campo para a inserção dos dados...)
+    
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DIO {
+  public static void main(String[] args) throws IOException {
+    Scanner leitor = new Scanner(System.in);
+    int cont = 0;
+    double media = 0;
+    double x = 0;
+    for (int i = 1; i < 7; i++) {
+      double num = leitor.nextDouble();
+
+      if(num >= 0){
+        cont++;
+        x = x + num;
+      }
+    }
+
+    media = x / cont;
+    System.out.println(cont + " valores positivos");
+    System.out.println(String.format("%.1f", media));
+  }
+}
+ */
